a3f14a3f923698660f8207281390770ba92df0c8
htamme <htamme@ut.ee>
2015-11-11 11:14:31 +0200

    user accounts

--- email_passwd.sh
+++ email_passwd.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
 upass=~/upass.csv
-sender_email="xxx@yyy.zz"
+sender_email="xxxx@yyyy.zz"
+url="https://686359387277.signin.aws.amazon.com/console"
 
 cat $upass | while read line;do
 	email="$(echo $line | awk -F ';' '{print $1}')"
 	pass="$(echo $line | awk -F ';' '{print $2}')"
 	(
-       		echo "Subject: AWS bootcamp credientials"
+       		echo "Subject: AWS bootcamp account"
        	 	echo "From:AWS Bootcamp Team <${sender_email}>"
 		echo "Return-Path: ${sender_email}"
 		echo "Replay-To: ${sender_email}"
        	 	echo "Content-type: text/plain"
        	 	echo "To: $email"
-       	 	echo "Hi,"
+       	 	echo "Hello"
 		echo ""
-		echo "Following password is needed during AWS bootcamp."
-		echo "Please change it."
+		echo "Here are your AWS account details. Please take a couple of minutes to follow the instructions below and set your account password."
 		echo ""
-       	 	echo "url: https://686359387277.signin.aws.amazon.com/console"
-       	 	echo "username: $email"
-       	 	echo "password: $pass" 
+		echo "Head over to the sign-in page: $url"
 		echo ""
-		echo "If you have any questions or problems, then please let us know."
-       	 	echo "AWS Bootcamp Team"
+       	 	echo "User Name: $email"
+       	 	echo "Temporary Login Password: $pass" 
+		echo "(Note that you have to leave "I have an MFA token" unchecked.)"
+		echo ""
+		echo "After signing in you'll be prompted to change your password."
+       	 	echo ""
+		echo "AWS Bootcamp Team"
 	)|/usr/sbin/sendmail -t -f${sender_email}
 done
